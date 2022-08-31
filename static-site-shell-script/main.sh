e42f076e06a173fc8821ceabdc3af6b572805aa8
caickdias <caickandrade0@gmail.com>
2022-08-31 16:48:04 -0300

    added simple html

--- static-site-shell-script/main.sh
+++ static-site-shell-script/main.sh
@@ -4,4 +4,15 @@ echo What is your site name?
 read SITE_NAME
 echo Site name is $SITE_NAME
 
-echo "$SITE_NAME" > index.html
+HTML="<!DOCTYPE html>
+<html>
+	<head>
+		<title>${SITE_NAME}</title>
+	</head>
+
+	<body>		
+		<h1>$SITE_NAME</h1>		
+	</body>
+</html>"
+
+echo "$HTML" > index.html
